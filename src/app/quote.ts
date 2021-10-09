dbc9ff5a01739957495e34113c9582c8d6fd01db
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-09 10:12:20 +0300

    Initialize quote constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,14 @@
 export class Quote {
+        public upvotes:number;
+        public  downvotes:number;
+        public datePosted:any;
+
+        constructor(public name:string, public content:string,public author:string,){
+            this.name = name;
+            this.content = content;
+            this.upvotes = 0;
+            this.downvotes = 0;
+            this.author = author;
+            this.datePosted = new Date();
+        }
 }
